d3725ef5dfcbb8db1b129920933cda7da0d1be4d
HardCPP <hardcpp@gmail.com>
2016-06-11 02:08:37 +0200

    Add packet type UI

---
+++ scripts/PacketStudio/Views/PacketTypes.lua
@@ -0,0 +1,71 @@
+-- Packet studio
+-- Copyright hardcpp@gmail.com 2016-2017
+-- https://github.com/hardcpp/PacketStudio
+
+PacketStudio.Views.PacketTypes = {}
+PacketStudio.Views.PacketTypes.Visible = false
+
+local g_PacketTypeFilter = ""
+
+-- =======================================================
+-- Render function
+-- =======================================================
+function PacketStudio_PacketTypes_MainDraw()
+    if PacketStudio.Views.PacketTypes.Visible == false then
+        return
+    end
+
+    local l_MainWindow;
+    _, l_MainWindow = ImGui.Begin("PacketStudio::PacketTypes", true, ImVec2(600, 400), -1.0)
+    if l_MainWindow then
+        ImGui.Text("Filter :")
+        ImGui.SameLine();
+        _, g_PacketTypeFilter = ImGui.InputText("", g_PacketTypeFilter)
+        ImGui.Separator()
+
+        if ImGui.CollapsingHeader("Results", "id_packet_types_result", true) then
+            ImGui.Columns(3, "packet_types")
+            ImGui.SetColumnOffset(1, 100)
+            ImGui.SetColumnOffset(2, 300)
+            ImGui.SetColumnOffset(3, 800)
+            for l_Key,l_Value in pairs(PacketStudio.PacketTypes) do
+                local l_OpcodeHex 		= "0x" .. Helpers.NumToHex(l_Value[1], 4)
+                local l_DirectionStr 	= "SERVER_TO_CLIENT"
+
+                if l_Value[2] == GlobalEnums.PacketDirection.CLIENT_TO_SERVER then
+                    l_DirectionStr = "SERVER_TO_CLIENT"
+                end
+
+                if g_PacketTypeFilter:len() ~= 0 then
+                    local l_MatchA = string.find(l_OpcodeHex, g_PacketTypeFilter:lower()) and true or false
+                    local l_MatchB = string.find(l_DirectionStr, g_PacketTypeFilter:lower()) and true or false
+                    local l_MatchC = string.find(l_Value[3]:lower(), g_PacketTypeFilter:lower()) and true or false
+
+                    if not l_MatchA and not l_MatchB and not l_MatchC then
+                        goto packet_types_continue
+                    end
+                end
+
+                ImGui.Text(l_OpcodeHex)
+                ImGui.NextColumn()
+                ImGui.Text(l_DirectionStr)
+                ImGui.NextColumn()
+                ImGui.Text(tostring(l_Value[3]))
+                ImGui.NextColumn()
+
+                ::packet_types_continue::
+            end
+            ImGui.Columns(1)
+        end
+
+        ImGui.End()
+    else
+        PacketStudio.Views.PacketTypes.Visible = false
+    end
+end
+
+-- =======================================================
+-- Callbacks registration
+-- =======================================================
+
+Pyx.Scripting.CurrentScript:RegisterCallback("ImGui.OnRender", PacketStudio_PacketTypes_MainDraw)
